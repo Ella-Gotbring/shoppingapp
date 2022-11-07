3ee459aea5a39849f4d80657c3080789ce467e8b
Ella-Gotbring <54885982+Ella-Gotbring@users.noreply.github.com>
2022-11-07 13:36:51 +0100

    Initial Commit

---
+++ shopping/shoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingApp.swift
+//  shopping
+//
+//  Created by Ella Götbring on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct shoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
