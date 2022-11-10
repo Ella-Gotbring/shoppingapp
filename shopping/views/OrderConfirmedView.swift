4810b563d3e0f00e262ef67de9e00d16a86d4fc6
Ella-Gotbring <54885982+Ella-Gotbring@users.noreply.github.com>
2022-11-10 11:45:58 +0100

    added order confirmation

--- shopping/views/OrderConfirmedView.swift
+++ shopping/views/OrderConfirmedView.swift
@@ -8,8 +8,20 @@
 import SwiftUI
 
 struct OrderConfirmedView: View {
+    @State var email = ""
     var body: some View {
-        Text("order confirmation")
+        VStack{
+            Text("Thank you!").font(.largeTitle).bold()
+            
+                .padding(10)
+            
+            Text("A order confirmation has been sent out to your email. Since you're here, sign up to get discounts ")
+            
+            NavigationLink(destination: LoginView(), label: {
+                Text("back to login")
+            })
+
+       
     }
 }
 
@@ -17,4 +29,5 @@ struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         OrderConfirmedView()
     }
+}
 }
